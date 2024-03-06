de85e396653bae74db82b0ad7e5f8963ead7fb85
vbhavsar16 <imvivek1695@gmail.com>
2024-03-06 02:45:52 -0500

    2D Heat PINN: Setup

---
+++ PINN_2D_Heat.py
@@ -0,0 +1,11 @@
+import deepxde as dde
+from deepxde.backend import tf
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+from tqdm import tqdm
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
